86e23103be8fca709ac9ce41df2b24f8704f7f44
Kamran Aghayev <kamran.aghayev@todor3d.tech>
2025-07-29 13:18:20 +0400

    add debounce filter

--- src/features/dashboard/ui/DashboardPage.tsx
+++ src/features/dashboard/ui/DashboardPage.tsx
@@ -1,19 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsersThunk } from "@/features/dashboard/model/thunks";
 import { RootState, AppDispatch } from "@/app/store";
 import { Pagination } from "./Pagination";
 import { UserModal } from "@/features/user/ui/UserModal";
 import { setSelectedUser } from "@/features/user/model/slice";
+import { useDebounce } from "@/shared/lib/hooks/useDebounce";
 
 const DashboardPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const { users, isLoading, error } = useSelector(
     (state: RootState) => state.dashboard
   );
+  const [searchTerm, setSearchTerm] = useState("");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
   useEffect(() => {
     dispatch(fetchUsersThunk(1));
   }, [dispatch]);
+
+  const filteredUsers = useMemo(() => {
+    if (!debouncedSearchTerm.trim()) return users;
+
+    return users.filter((user) => {
+      const fullName = `${user.first_name} ${user.last_name}`.toLowerCase();
+      return (
+        fullName.includes(debouncedSearchTerm.toLowerCase()) ||
+        user.email.toLowerCase().includes(debouncedSearchTerm.toLowerCase())
+      );
+    });
+  }, [users, debouncedSearchTerm]);
 
   return (
     <div style={{ padding: "20px" }}>
@@ -21,10 +37,19 @@ const DashboardPage = () => {
 
       {isLoading && <p>Загрузка пользователей...</p>}
       {error && <p style={{ color: "red" }}>{error}</p>}
+
+      <input
+        type="text"
+        placeholder="Поиск пользователей..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        style={{ marginBottom: "20px", padding: "8px", width: "100%" }}
+      />
+
       <ul
         style={{ display: "grid", gap: "10px", listStyle: "none", padding: 0 }}
       >
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <li
             key={user.id}
             onClick={() => dispatch(setSelectedUser(user))}
@@ -35,7 +60,7 @@ const DashboardPage = () => {
               padding: "10px",
               border: "1px solid #ccc",
               borderRadius: "8px",
-              cursor: "pointer"
+              cursor: "pointer",
             }}
           >
             <img
@@ -56,6 +81,7 @@ const DashboardPage = () => {
           </li>
         ))}
       </ul>
+
       <UserModal />
       <Pagination />
     </div>
